a7b4631cf9c54c2d7c702cedf5362444572cfaae
tobias-edwards <tobiasrichardedwards@gmail.com>
2021-12-15 22:01:19 +0000

    feat: QR Code generatorapp

---
+++ qr-code/snowpack.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: ['@snowpack/plugin-react-refresh'],
+  knownEntrypoints: ['react-is'],
+};
